b4bbfa848d579f42106b120e107abade7810b9c6
cuongtq37 <trancuong.02092000@gmail.com>
2021-03-11 00:35:06 +0700

    create organization

---
+++ db/migrate/20210310172837_create_organization.rb
@@ -0,0 +1,11 @@
+class CreateOrganization < ActiveRecord::Migration[6.0]
+  def change
+    create_table :organizations do |t|
+      t.string :name
+      t.string :avatar
+      t.string :address
+      t.string :description
+      t.string :page
+    end
+  end
+end
